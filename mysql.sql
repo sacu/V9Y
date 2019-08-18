135c54cd62f9ee816f10430ce617a8e657fec4cb
sacu <saculer@hotmail.com>
2019-08-18 22:46:48 +0800

    sql

---
+++ mysql.sql
@@ -0,0 +1,33 @@
+﻿
+CREATE DATABASE IF NOT EXISTS `sa_fish_db`;
+USE `sa_fish_db`;
+set names 'utf8';
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- dt 普通可变数据表
+-- st 静态数据表
+
+-- 以下为后台管理
+DROP TABLE IF EXISTS `sa_dt_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sa_dt_user` (
+	`id` int(4) primary key not null auto_increment COMMENT '主键',
+	`username` varchar(32) not null COMMENT '帐号',
+	`password` varchar(32) not null COMMENT '密码',
+	`nickname` varchar(32) not null COMMENT '昵称',
+	`coin` int default 0 COMMENT '金币',
+	`diamond` int default 0 COMMENT '钻石',
+	`appid` varchar(32) not null COMMENT 'appid',
+	`appsecret` varchar(32) not null COMMENT 'appsecret'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='管理员表';
+/*!40101 SET character_set_client = @saved_cs_client */;
